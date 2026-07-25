f81db9d4c23d597ecab47b3d9f9dd79c8e7812eb
Shivanand <shivanandmugali882@gmail.com>
2026-07-25 23:48:23 +0530

    Palindrome

--- Strings/Exmethods.java
+++ Strings/Exmethods.java
@@ -2,8 +2,12 @@ package Strings;
 import java.util.Arrays;
 public class Exmethods {
     public static void main(String[] args) {
-        String name = "Shivanand Mugali";
+        String name = "Shivanand R Mugali";
         System.out.println(Arrays.toString(name.toCharArray()));
         System.out.println(name.charAt(0));
+        String a = "            ShivanadM          ";
+        System.out.println(a.strip());
+        System.out.println(name.indexOf("M"));
+        System.out.println(Arrays.toString(name.split(" ")));
     }
 }
---
+++ Strings/Explain.java
@@ -0,0 +1,24 @@
+package Strings;
+
+public class Explain {
+    public static void main(String[] args) {
+        String str = "abcddcba";
+        System.out.println(palindrome(str));
+        
+    }
+    static boolean palindrome(String str){
+        if(str == null || str.length() == 0){
+            return true;
+        }
+        str = str.toLowerCase();
+        for(int i = 0; i< str.length()/2; i++){
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-1-i);
+            if(start != end){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
