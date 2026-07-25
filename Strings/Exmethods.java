0b302db7a91892875d57e348ead4ee6a3ff11a11
Shivanand <shivanandmugali882@gmail.com>
2026-07-25 23:18:59 +0530

    toChar

---
+++ Strings/Exmethods.java
@@ -0,0 +1,8 @@
+package Strings;
+import java.util.Arrays;
+public class Exmethods {
+    public static void main(String[] args) {
+        String name = "Shivanand Mugali";
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+}
