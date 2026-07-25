df5b99678ded3d3e819a586b5aa1df6b4243a0eb
Shivanand <shivanandmugali882@gmail.com>
2026-07-25 23:22:24 +0530

    String method operations

--- Strings/Exmethods.java
+++ Strings/Exmethods.java
@@ -4,5 +4,6 @@ public class Exmethods {
     public static void main(String[] args) {
         String name = "Shivanand Mugali";
         System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.charAt(0));
     }
 }
